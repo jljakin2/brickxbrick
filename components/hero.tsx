2481eb779d97346a0dd477d4ff275a626d79c977
Jeff Jakinovich <53125676+jljakin2@users.noreply.github.com>
2024-05-20 09:18:18 -0400

    updated the hero to reflect brand styling

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import Avatar01 from '@/public/images/avatar-01.jpg'
-import Avatar02 from '@/public/images/avatar-02.jpg'
-import Avatar03 from '@/public/images/avatar-03.jpg'
-import Avatar04 from '@/public/images/avatar-04.jpg'
-import Client01 from '@/public/images/client-01.svg'
-import Client02 from '@/public/images/client-02.svg'
-import Client03 from '@/public/images/client-03.svg'
-import Client04 from '@/public/images/client-04.svg'
-import iPhone from '@/public/images/iphone.png'
+import Link from "next/link";
+import Image from "next/image";
+import Avatar01 from "@/public/images/avatar-01.jpg";
+import Avatar02 from "@/public/images/avatar-02.jpg";
+import Avatar03 from "@/public/images/avatar-03.jpg";
+import Avatar04 from "@/public/images/avatar-04.jpg";
+import Client01 from "@/public/images/client-01.svg";
+import Client02 from "@/public/images/client-02.svg";
+import Client03 from "@/public/images/client-03.svg";
+import Client04 from "@/public/images/client-04.svg";
+import iPhone from "@/public/images/iphone.png";
 
 export default function Hero() {
   return (
@@ -19,41 +19,66 @@ export default function Hero() {
           <div className="relative max-w-xl mx-auto md:max-w-none text-center md:text-left flex flex-col md:flex-row">
             {/* Background */}
             <div
-              className="absolute inset-0 bg-gradient-to-br from-slate-900 to-slate-800 rounded-3xl -mx-20 -z-10 overflow-hidden mb-12 mt-0 md:mb-0"
+              className="absolute inset-0 bg-[url('/images/i.png')] bg-cover rounded-3xl -mx-20 -z-10 overflow-hidden mb-12 mt-0 md:mb-0 filter brightness-7"
               aria-hidden="true"
             >
               {/* Illustration */}
               <div className="absolute -top-16 left-1/2 -translate-x-1/3 md:-translate-x-1/2 pointer-events-none -z-10 blur-2xl">
-                <svg xmlns="http://www.w3.org/2000/svg" width="2106" height="1327">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2106"
+                  height="1327"
+                >
                   <defs>
-                    <filter id="hi-a" width="133.3%" height="131.3%" x="-16.7%" y="-15.6%" filterUnits="objectBoundingBox">
+                    <filter
+                      id="hi-a"
+                      width="133.3%"
+                      height="131.3%"
+                      x="-16.7%"
+                      y="-15.6%"
+                      filterUnits="objectBoundingBox"
+                    >
                       <feGaussianBlur in="SourceGraphic" stdDeviation="0" />
                     </filter>
-                    <filter id="hi-b" width="133.3%" height="131.3%" x="-16.7%" y="-15.6%" filterUnits="objectBoundingBox">
+                    <filter
+                      id="hi-b"
+                      width="133.3%"
+                      height="131.3%"
+                      x="-16.7%"
+                      y="-15.6%"
+                      filterUnits="objectBoundingBox"
+                    >
                       <feGaussianBlur in="SourceGraphic" stdDeviation="0" />
                     </filter>
-                    <filter id="hi-c" width="159.9%" height="145%" x="-29.9%" y="-22.5%" filterUnits="objectBoundingBox">
+                    <filter
+                      id="hi-c"
+                      width="159.9%"
+                      height="145%"
+                      x="-29.9%"
+                      y="-22.5%"
+                      filterUnits="objectBoundingBox"
+                    >
                       <feGaussianBlur in="SourceGraphic" stdDeviation="0" />
                     </filter>
                   </defs>
                   <g fill="none" fillRule="evenodd">
                     <path
-                      fill="#6D28D9"
-                      fillOpacity=".72"
+                      fill="#ef3047"
+                      fillOpacity=".5"
                       d="M1110.164 893.257C1191.124 1079.102 1484 839.962 1484 626.315S883.228 0 669.507 0s40.54 412.668 40.54 626.315c0 213.647 319.156 81.096 400.117 266.942Z"
                       filter="url(#hi-a)"
                       transform="translate(0 -605)"
                     />
                     <path
-                      fill="#67E8F9"
-                      fillOpacity=".64"
+                      fill="#ef3047"
+                      fillOpacity=".4"
                       d="M1732.164 1753.257c80.96 185.845 373.836-53.295 373.836-266.942S1505.228 860 1291.507 860s40.54 412.668 40.54 626.315c0 213.647 319.156 81.096 400.117 266.942Z"
                       filter="url(#hi-b)"
                       transform="translate(0 -605)"
                     />
                     <path
-                      fill="#F472B6"
-                      fillOpacity=".8"
+                      fill="#fff"
+                      fillOpacity=".5"
                       d="M1191.108 871C1338.988 871 1631 635.765 1631 487.507 1631 339.248 1625.874 205 1477.994 205s-267.76 120.187-267.76 268.445c0 148.259-167.006 397.555-19.126 397.555Z"
                       filter="url(#hi-c)"
                       transform="translate(0 -605)"
@@ -66,10 +91,14 @@ export default function Hero() {
             {/* Content */}
             <div className="md:w-[640px] py-12 md:py-20 md:min-h-[480px]">
               {/* Copy */}
-              <h1 className="h1 font-hkgrotesk text-slate-100 mb-6" data-aos="fade-right" data-aos-delay="100">
-                Learn how to start a business the{' '}
-                <span className="font-permanent-marker text-blue-500 font-normal whitespace-nowrap">
-                  right{' '}
+              <h1
+                className="h1 font-hkgrotesk text-slate-100 mb-6"
+                data-aos="fade-right"
+                data-aos-delay="100"
+              >
+                Learn to build and sell.{" "}
+                <span className="font-permanent-marker text-red-500 font-normal whitespace-nowrap">
+                  Become{" "}
                   <span className="inline-flex relative">
                     <svg
                       className="absolute right-0 top-full mt-1 max-w-none -z-10"
@@ -85,13 +114,18 @@ export default function Hero() {
                         opacity=".32"
                       />
                     </svg>
-                    way
+                    unstoppable
                   </span>
-                  <span className="text-slate-100">.</span>
+                  <span className="text-red-500">.</span>
                 </span>
               </h1>
-              <p className="text-lg text-slate-400 mb-8" data-aos="fade-right" data-aos-delay="200">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+              <p
+                className="text-lg text-white mb-8"
+                data-aos="fade-right"
+                data-aos-delay="200"
+              >
+                Our landing page template works on all devices, so you only have
+                to set it up once, and get beautiful results forever.
               </p>
               {/* Buttons */}
               <div
@@ -100,25 +134,58 @@ export default function Hero() {
                 data-aos-delay="300"
               >
                 <div>
-                  <Link className="btn text-white bg-blue-500 hover:bg-blue-600 group shadow-sm" href="/subscribe">
-                    Subscribe - Free{' '}
-                    <span className="tracking-normal text-blue-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
+                  <Link
+                    className="btn text-white bg-red-500 group shadow-sm"
+                    href="/subscribe"
+                  >
+                    Subscribe - Free{" "}
+                    <span className="tracking-normal text-white group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
                       -&gt;
                     </span>
                   </Link>
                 </div>
-                <div className="sm:flex sm:items-center sm:justify-center space-y-2 sm:space-y-0 sm:space-x-3">
+                {/* <div className="sm:flex sm:items-center sm:justify-center space-y-2 sm:space-y-0 sm:space-x-3">
                   <div className="inline-flex -space-x-3 -ml-0.5">
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar01} width={28} height={28} alt="Avatar 01" />
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar02} width={28} height={28} alt="Avatar 02" />
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar03} width={28} height={28} alt="Avatar 03" />
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar04} width={28} height={28} alt="Avatar 04" />
+                    <Image
+                      className="rounded-full border-2 border-slate-800 box-content"
+                      src={Avatar01}
+                      width={28}
+                      height={28}
+                      alt="Avatar 01"
+                    />
+                    <Image
+                      className="rounded-full border-2 border-slate-800 box-content"
+                      src={Avatar02}
+                      width={28}
+                      height={28}
+                      alt="Avatar 02"
+                    />
+                    <Image
+                      className="rounded-full border-2 border-slate-800 box-content"
+                      src={Avatar03}
+                      width={28}
+                      height={28}
+                      alt="Avatar 03"
+                    />
+                    <Image
+                      className="rounded-full border-2 border-slate-800 box-content"
+                      src={Avatar04}
+                      width={28}
+                      height={28}
+                      alt="Avatar 04"
+                    />
                   </div>
-                  <div className="text-xs text-slate-500 font-medium">+120K</div>
-                </div>
+                  <div className="text-xs text-slate-500 font-medium">
+                    +120K
+                  </div>
+                </div>*/}
               </div>
               {/* Press logos */}
-              <div className="max-w-xs mx-auto sm:max-w-none" data-aos="fade-right" data-aos-delay="400">
+              {/* <div
+                className="max-w-xs mx-auto sm:max-w-none"
+                data-aos="fade-right"
+                data-aos-delay="400"
+              >
                 <div className="inline-flex flex-wrap items-center justify-center -my-2 -mx-4 md:-mx-6">
                   <div className="w-1/2 sm:w-auto flex justify-center py-2 px-4 md:px-6">
                     <Image src={Client01} width={82} alt="Client 01" />
@@ -133,18 +200,25 @@ export default function Hero() {
                     <Image src={Client04} width={82} alt="Client 04" />
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
 
             {/* Image */}
             <div className="max-w-sm mx-auto md:max-w-none md:absolute md:left-[40rem] md:top-1/2 md:-translate-y-1/2 md:ml-8 lg:ml-20 xl:ml-32">
               <div data-aos="fade-left" data-aos-duration="1100">
-                <Image src={iPhone} className="md:max-w-none" width={298} height={601} priority alt="iPhone" />
+                <Image
+                  src={iPhone}
+                  className="md:max-w-none"
+                  width={298}
+                  height={601}
+                  priority
+                  alt="iPhone"
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
