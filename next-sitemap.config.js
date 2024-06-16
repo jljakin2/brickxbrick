3d285a84acdad20b4972b821f2a4e2653683ba5a
Jeff Jakinovich <53125676+jljakin2@users.noreply.github.com>
2024-06-16 10:36:21 -0400

    update sitemap stuff so it handles redirects correctly

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://brickbybrick.fm", // Replace with your site URL
+  siteUrl: "https://www.brickbybrick.fm", // Ensure this is updated
   generateRobotsTxt: true,
   sitemapSize: 5000, // Adjust as needed
   exclude: ["/404"], // Exclude any pages you don't want in the sitemap
@@ -7,7 +7,6 @@ module.exports = {
   priority: 0.7, // Adjust as needed
   outDir: "public", // Ensure the sitemap is generated in the public directory
   transform: async (config, path) => {
-    // Customize the URLs if needed
     return {
       loc: path, // Use the default path
       changefreq: config.changefreq,
@@ -15,7 +14,4 @@ module.exports = {
       lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
     };
   },
-  // additionalPaths: async (config) => [
-  //   await config.transform(config, "/additional-page"), // Add additional pages if needed
-  // ],
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://www.brickbybrick.fm", // Redirect to the preferred URL
+        permanent: true,
+        statusCode: 308, // Use 308 Permanent Redirect
+      },
+    ];
+  },
+};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig);
